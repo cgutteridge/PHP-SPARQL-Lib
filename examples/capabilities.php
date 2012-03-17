264d346ba7b1bcb470058ffab73fe4dfbf3ae416
Christopher Gutteridge <cjg@ecs.soton.ac.uk>
2012-03-17 07:19:21 +0000

    more examples

--- examples/capabilities.php
+++ examples/capabilities.php
@@ -5,8 +5,17 @@ table.capabilities td { padding-right: 1em; }
 </style>
 <?
 require_once( "sparqllib.php" );
-
+print "<p>These are tests against PUBLIC endpoints. They may support LOAD when credentials are supplied.</p>";
+print "<p>ARC2</p>";
 capability_table( "http://programme.ecs.soton.ac.uk/glastonbury/2011/sparql" );
+print "<p>4store</p>";
+capability_table( "http://sparql.data.southampton.ac.uk/" );
+#print "<p>joseki 3</p>";
+#capability_table( "http://jena.hpl.hp.com:3040/backstage" );
+print "<p>virtuoso</p>";
+capability_table( "http://data.semanticweb.org/sparql" );
+print "<p>bigfoot</p>";
+capability_table( "http://services.data.gov.uk/reference/sparql" );
 
 function capability_table($endpoint)
 {

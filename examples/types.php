47378342a9856f478c21738426b1c405c7cf9539
Christopher Gutteridge <cjg@ecs.soton.ac.uk>
2012-03-17 07:52:40 +0000

    added types example

---
+++ examples/types.php
@@ -0,0 +1,39 @@
+<?
+require_once( "sparqllib.php" );
+
+$data = sparql_get( 
+	"http://rdf.ecs.soton.ac.uk/sparql/",
+	"
+PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+SELECT * WHERE { ?person a foaf:Person . ?person foaf:name ?name } LIMIT 5
+" );
+if( !isset($data) )
+{
+	print "<p>Error: ".sparql_errno().": ".sparql_error()."</p>";
+}
+
+print "<table border='1'>";
+print "<tr>";
+foreach( $data->fields() as $field )
+{
+	print "<th>$field</th>";
+	print "<th>$field.type</th>";
+	print "<th>$field.datatype</th>";
+	print "<th>$field.language</th>";
+}
+print "</tr>";
+foreach( $data as $row )
+{
+	print "<tr>";
+	foreach( $data->fields() as $field )
+	{
+		print "<td>$row[$field]</td>";
+		print "<td>".$row["$field.type"]."</td>";
+		print "<td>".@$row["$field.datatype"]."</td>";
+		print "<td>".@$row["$field.language"]."</td>";
+	}
+	print "</tr>";
+}
+print "</table>";
+
+
